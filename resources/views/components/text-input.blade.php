10b1342d99acf06b69c41ddeecc8f90c30251b73
aldrvk <aldriknoelsianipar@gmail.com>
2025-11-19 11:09:54 +0700

    login dan register bisa darkmode

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,4 +1,10 @@
 @props(['disabled' => false])
 
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm']) }}>
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'w-full bg-gray-800 border-gray-700 text-gray-200 placeholder-gray-400 focus:border-blue-500 focus:ring-blue-500 rounded-md shadow-sm']) !!}>
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge([
+    'class' => 'border-gray-300 dark:border-primary-700 ' . 
+    'dark:bg-primary-850 ' . 
+    'dark:text-gray-100 ' .  
+    'focus:border-primary-500 dark:focus:border-primary-400 ' . 
+    'focus:ring-primary-500 dark:focus:ring-primary-400 ' .     
+    'rounded-md shadow-sm'
+]) !!}>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,16 +15,15 @@
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-
 <body class="font-sans text-gray-900 antialiased">
-    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-imi-primary">
+    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-primary-950">
         <div>
             <a href="/">
-                <x-application-logo class="w-25 h-32 fill-current text-gray-500" />
+                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
             </a>
         </div>
 
-        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-primary-900 shadow-xl overflow-hidden sm:rounded-lg">
+        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-primary-900 shadow-md overflow-hidden sm:rounded-lg">
             {{ $slot }}
         </div>
     </div>
